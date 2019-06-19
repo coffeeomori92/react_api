2bf12b2b2c472517e4dc1fad06b736105580b6b7
coffeemori92 <coffeemori92@naver.com>
2019-06-19 10:11:51 +0900

    Completed a tic-tac-toe game

--- src/components/Game.js
+++ src/components/Game.js
@@ -57,11 +57,12 @@ class Game extends Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
-            xIsNext : true
+            xIsNext : true,
+            stepNumber : 0
         };
     }
     handleClick(i){
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current  = history[history.length - 1];
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]){
@@ -72,14 +73,31 @@ class Game extends Component {
             history : history.concat([{
                 squares : squares
             }]),
-            xIsNext : !this.state.xIsNext
+            xIsNext : !this.state.xIsNext,
+            stepNumber : history.length
+        });
+    }
+    jumpTo(step){
+        this.setState({
+            stepNumber : step,
+            xIsNext : (step % 2) === 0
         });
     }
     render(){
         const history = this.state.history;
-        const curret  = history[history.length - 1];
+        const curret  = history[this.state.stepNumber];
         const winner  = calculateWinner(curret.squares);
         
+        const moves = history.map((step, move) => {
+            const desc = move ? 
+                'Go to move #' + move : 
+                'Go to game start';
+            return (
+                <li key = {move}>
+                    <button onClick = {() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
         let status;
         if(winner){
             status = 'Winner : ' + winner;
@@ -96,7 +114,7 @@ class Game extends Component {
                 </div>
                 <div className = "game-info">
                     <div>{status}</div>
-                    <ol>{/*TODO*/}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
